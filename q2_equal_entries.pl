2848880328675f90994440efb8b702f3a0472952
tckillam <tckillam@gmail.com>
2024-10-02 17:23:23 -0400

    Update q2_equal_entries.pl
    I could not for the life of me get any of my questions to work (although I feel close on question 5) so I thought I could help with 2. If you also did it and it works, then we know we are officially done question 2.

--- q2_equal_entries.pl
+++ q2_equal_entries.pl
@@ -2,8 +2,8 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
+%%%%% NAME: Shaghayegh Dehghanisanij
+%%%%% NAME: Theresa Killam
 %%%%% NAME:
 %
 % Add the required rules in the corresponding sections. 
@@ -15,4 +15,16 @@
 %%%%% SECTION: equalEntries
 %%%%% Put your rules for equalEntries below
 
-equalEntries(X,Y).
+equalEntries([X], [X], [true]).
+equalEntries([X], [Y], [false]) :- not (X = Y).
+equalEntries([H|Tail1], [H|Tail2], [true|Rest]) :- equalEntries(Tail1, Tail2, Rest).
+equalEntries([H1|Tail1], [H2|Tail2], [false|Rest]) :- not (H1 = H2), equalEntries(Tail1, Tail2, Rest).
+
+/*
+% equalEntries([1], [1], [true]).  --> works
+% equalEntries([1], [2], [true]).  --> works
+% equalEntries([1], [2], [false]).  --> works
+
+% equalEntries([a,b,c], [a,b,c], X). --> works
+% equalEntries([a, b, [c], d, [e, f, g]], [1, b, c, d, [e, f, g]], [false, true, false, true, true]) --> works.
+*/
