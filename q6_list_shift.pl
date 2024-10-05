edc93127bbb65dfd668dff9edb29093d1a6c129d
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-10-05 16:27:00 -0400

    q6_list_shift.pl works again
    extractFirstV => appendAtEnd

--- q6_list_shift.pl
+++ q6_list_shift.pl
@@ -16,48 +16,53 @@
 %%%%% Put your rules for listShift and any helper predicates below
 
 
-% NEXT to list
-next_to_list(nil, []).
-next_to_list(next(H, T), [H|List]) :-
-    next_to_list(T, List).
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: Shaghayegh Dehghanisanij
+%%%%% NAME: 
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the already included comment lines below
+%
 
-% list to NEXT
-list_to_next([], nil).
-list_to_next([H|T], next(H, Next)) :-
-    list_to_next(T, Next).
-
-% Get the length of list
-list_length([], 0).
-list_length([_|T], L) :-
-    list_length(T, L1),
-    L is L1 + 1.
-
-
-% Base case: list has one element
-listShiftHelper(List, 0, List).
+%%%%% SECTION: listShift
+%%%%% Put your rules for listShift and any helper predicates below
 
 
-% Case 2: Shift >= len list
-listShiftHelper(List, V, List) :-
-    list_length(List, Len),
-    V >= Len.
+% Base case: NO SHIFT
+listShift(List, 0, List).
 
-% case 3: Shift < len list
-listShiftHelper([H | T], V, Result) :-
-    NewV is V - 1,
-    shifter([H], T, NewList),
-    listShiftHelper(NewList, NewV, Result).
+% If SHIFT > LENGTH
+listShift(List, V, List) :-
+    lengthOfList(List, Length),
+    V >= Length.
+
+% Recursive case
+listShift(next(Head, Tail), V, Result) :-
+    V > 0,
+    lengthOfList(next(Head, Tail), Length),
+    V < Length,
+    extractFirstV(next(Head, Tail), V, Extracted, Remaining),
+    appendAtEnd(Remaining, Extracted, Result).
+
+extractFirstV(List, 0, nil, List).
+extractFirstV(next(Head, Tail), V, next(Head, ExtractedTail), Remaining) :-
+    V > 0,
+    V1 is V - 1,
+    extractFirstV(Tail, V1, ExtractedTail, Remaining).
+
+appendAtEnd(nil, List, List).
+appendAtEnd(next(Head, Tail), List, next(Head, ResultTail)) :-
+    appendAtEnd(Tail, List, ResultTail).
 
 
-shifter([X],[T], [T,X]).
-shifter([X], [H|T], [H|Y]) :- shifter([X],T,Y).
-
-
-% final predicate
-% conver NEXT to list => perform SHIFT => conver list to NEXT
-listShift(Next, V, ResultNext) :-
-    next_to_list(Next, List),
-    listShiftHelper(List, V, ShiftedList),
-    list_to_next(ShiftedList, ResultNext).
-
+lengthOfList(nil, 0).
+lengthOfList(next(_, Tail), Length) :-
+    lengthOfList(Tail, TailLength),
+    Length is TailLength + 1.
 
