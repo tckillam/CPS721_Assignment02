6e675c51734d7f4836fda0b1aa4092642b55e782
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-10-05 16:27:49 -0400

    Update q6_list_shift.pl

--- q6_list_shift.pl
+++ q6_list_shift.pl
@@ -12,27 +12,6 @@
 % You may add additional comments as you choose but DO NOT MODIFY the already included comment lines below
 %
 
-%%%%% SECTION: listShift
-%%%%% Put your rules for listShift and any helper predicates below
-
-
-% Enter the names of your group members below.
-% If you only have 2 group members, leave the last space blank
-%
-%%%%%
-%%%%% NAME: Shaghayegh Dehghanisanij
-%%%%% NAME: 
-%%%%% NAME:
-%
-% Add the required rules in the corresponding sections. 
-% If you put the rules in the wrong sections, you will lose marks.
-%
-% You may add additional comments as you choose but DO NOT MODIFY the already included comment lines below
-%
-
-%%%%% SECTION: listShift
-%%%%% Put your rules for listShift and any helper predicates below
-
 
 % Base case: NO SHIFT
 listShift(List, 0, List).
