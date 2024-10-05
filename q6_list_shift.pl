4864b92f6170c17d2cc55ac22c484f4540fc589a
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-10-05 15:11:07 -0400

    q6_list_shift.pl works!
    % conver NEXT to list => perform SHIFT => conver list to NEXT

--- q6_list_shift.pl
+++ q6_list_shift.pl
@@ -16,40 +16,48 @@
 %%%%% Put your rules for listShift and any helper predicates below
 
 
-helper(next(Head, nil), [Head]).
+% NEXT to list
+next_to_list(nil, []).
+next_to_list(next(H, T), [H|List]) :-
+    next_to_list(T, List).
 
-helper(next(Head, Tail), List) :- 
-    List is [Head|Y], helper(next(Tail, X), List).
+% list to NEXT
+list_to_next([], nil).
+list_to_next([H|T], next(H, Next)) :-
+    list_to_next(T, Next).
 
-/**
+% Get the length of list
+list_length([], 0).
+list_length([_|T], L) :-
+    list_length(T, L1),
+    L is L1 + 1.
 
-helper(next(7, next(1, nil)), List).
-next(7, next(1, nil)).
-
-1. next(5,  next(0, next(9, next(7, next(1, nil)))))'
-
-2. next(0, next(9, next(7, next(1, nil))))
-
-**/
-
-/**
-next(Head, Tail) :- not(Tail = nil), next()
-**/
-nextHelper(Head, nil) :-
-    [Head].
 
 % Base case: list has one element
-listShift(List, 0, List).
+listShiftHelper(List, 0, List).
 
-listShift([H | T], V, Result) :-
+
+% Case 2: Shift >= len list
+listShiftHelper(List, V, List) :-
+    list_length(List, Len),
+    V >= Len.
+
+% case 3: Shift < len list
+listShiftHelper([H | T], V, Result) :-
     NewV is V - 1,
     shifter([H], T, NewList),
-    listShift(NewList, NewV, Result).
+    listShiftHelper(NewList, NewV, Result).
 
-
-/**
-newList([1],[2,3], Result) --> [2,3,1]
-**/
 
 shifter([X],[T], [T,X]).
-shifter([X], [H|T], [H|Y]) :- shifter([X],T,Y). 
+shifter([X], [H|T], [H|Y]) :- shifter([X],T,Y).
+
+
+% final predicate
+% conver NEXT to list => perform SHIFT => conver list to NEXT
+listShift(Next, V, ResultNext) :-
+    next_to_list(Next, List),
+    listShiftHelper(List, V, ShiftedList),
+    list_to_next(ShiftedList, ResultNext).
+
+
